728990d4d769ac115555e4468eca7656585a39c9
mohamed <msa0back@gmail.com>
2025-03-21 23:11:14 +0200

    save

--- resources/views/User/favorites.blade.php
+++ resources/views/User/favorites.blade.php
@@ -9,45 +9,47 @@
     </x-slot>
 
     <div class="py-12">
-        @if ($products->isEmpty())
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg" style="padding: 50px;">
-                <center style="display: block">
-                    <h2 style="margin: 50px;">No Favorites Yet</h2>
-                    <div><a style="color: red;border: 1px solid red;padding: 8px;border-radius: 10px;"
-                            href="{{ route('Home') }}#down">Products</a></div>
-                </center>
-            </div>
-        @else
-            <section class="container">
-                @foreach ($products as $favorite)
-                    <div class="card">
-                        <div class="card-header">
-                            <livewire:favorite :roseId="$favorite->product->id" />
-                            <a href="{{ route('product.details', ['id' => $favorite->product->id]) }}"
-                                class="canonical">
-                                <div class="img-cont">
-                                    <img src="{{ 'storage/' . $favorite->product->path }}" alt="" />
-                                </div>
-                            </a>
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            @if ($products->isEmpty())
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg" style="padding: 50px;">
+                    <center style="display: block">
+                        <h2 style="margin: 50px;">No Favorites Yet</h2>
+                        <div><a style="color: red;border: 1px solid red;padding: 8px;border-radius: 10px;"
+                                href="{{ route('Home') }}#down">Products</a></div>
+                    </center>
+                </div>
+            @else
+                <section class="container">
+                    @foreach ($products as $favorite)
+                        <div class="card">
+                            <div class="card-header">
+                                <livewire:favorite :roseId="$favorite->product->id" />
+                                <a href="{{ route('product.details', ['id' => $favorite->product->id]) }}"
+                                    class="canonical">
+                                    <div class="img-cont">
+                                        <img src="{{ 'storage/' . $favorite->product->path }}" alt="" />
+                                    </div>
+                                </a>
 
-                            <div class="card-title">
-                                <h3> {{ $favorite->product->title }}</h3>
+                                <div class="card-title">
+                                    <h3> {{ $favorite->product->title }}</h3>
+                                </div>
+                            </div>
+                            <div class="card-footer">
+                                <p class="card-price">
+                                    @if ($favorite->product->discount !== 0)
+                                        <span
+                                            class="price-before"><del>{{ $favorite->product->price + $favorite->product->discount }}</del>
+                                            EGP</span>
+                                    @endif
+                                    <span class="price-after">{{ $favorite->product->price }} EGP</span>
+                                </p>
+                                <livewire:cart :roseId="$favorite->product->id" />
                             </div>
                         </div>
-                        <div class="card-footer">
-                            <p class="card-price">
-                                @if ($favorite->product->discount !== 0)
-                                    <span
-                                        class="price-before"><del>{{ $favorite->product->price + $favorite->product->discount }}</del>
-                                        EGP</span>
-                                @endif
-                                <span class="price-after">{{ $favorite->product->price }} EGP</span>
-                            </p>
-                            <livewire:cart :roseId="$favorite->product->id" />
-                        </div>
-                    </div>
-                @endforeach
-        @endif
-        </section>
+                    @endforeach
+            @endif
+            </section>
+        </div>
     </div>
 </x-app-layout>

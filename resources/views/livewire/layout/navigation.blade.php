5455227496b6740de3b3d8c16a74dba87321cb2b
mohamed <msa0back@gmail.com>
2025-03-21 15:36:15 +0200

    save

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -152,7 +152,7 @@ new class extends Component {
                 {{ __('About Ra3d') }}
             </x-responsive-nav-link>
         </div> --}}
-        @if (auth()->check() && auth()->user()->status == 0)
+        @if (auth()->check())
             <div class="pt-2 pb-3 space-y-1">
                 <x-responsive-nav-link :href="route('user.orders')" :active="request()->routeIs('user.orders')">
                     {{ __('Orders ') }}
